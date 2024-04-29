b101567bf4f90fbbe3b7e58d08634dc3bf4ac651
Vedprakash Kumar <131158289+Vedprakash321@users.noreply.github.com>
2024-04-29 21:28:08 +0000

    solution of useMemo

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,47 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import React, { useState, useMemo } from "react";
+import './../styles/App.css';
+
+const App = () => {
+  
+    const [count, setCount] = useState(0);
+    const [todos, setTodos] = useState([]);
+    const calculation = useMemo(() => expensiveCalculation(count), [count]);
+
+    const increment = () => {
+        setCount((c) => c + 1);
+    };
+    const addTodo = () => {
+        setTodos((t) => [...t, "New Todo"]);
+    };
+
+ 
+
+  return (
+    <div>
+       <div>
+        <h2>My Todos</h2>
+        {todos.map((todo, index) => {
+          return <p key={index}>{todo}</p>;
+        })}
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+        <h2>Expensive Calculation</h2>
+        {calculation}
+      </div>
+    </div>
+  )
+}
+
+const expensiveCalculation = (num) => {
+    console.log("Calculating...");
+    for (let i = 0; i < 1000000000; i++) {
+      num += 1;
+    }
+    return num;
+};
+export default App;
