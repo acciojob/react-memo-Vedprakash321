da5c1aa5f100fa446253761ad83cbaeaa3ac752f
Vedprakash Kumar <131158289+Vedprakash321@users.noreply.github.com>
2024-04-29 21:34:16 +0000

    after resolve testcase

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,51 @@
 
+// import React, { useState, useMemo } from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+  
+//     const [count, setCount] = useState(0);
+//     const [todos, setTodos] = useState([]);
+//     const calculation = useMemo(() => expensiveCalculation(count), [count]);
+
+//     const increment = () => {
+//         setCount((c) => c + 1);
+//     };
+//     const addTodo = () => {
+//         setTodos((t) => [...t, "New Todo"]);
+//     };
+
+ 
+
+//   return (
+//     <div>
+//        <div>
+//         <h2>My Todos</h2>
+//         {todos.map((todo, index) => {
+//           return <p key={index}>{todo}</p>;
+//         })}
+//         <button onClick={addTodo}>Add Todo</button>
+//       </div>
+//       <hr />
+//       <div>
+//         Count: {count}
+//         <button onClick={increment}>+</button>
+//         <h2>Expensive Calculation</h2>
+//         {calculation}
+//       </div>
+//     </div>
+//   )
+// }
+
+// const expensiveCalculation = (num) => {
+//     console.log("Calculating...");
+//     for (let i = 0; i < 1000000000; i++) {
+//       num += 1;
+//     }
+//     return num;
+// };
+// export default App;
+
 import React, { useState, useMemo } from "react";
 import './../styles/App.css';
 
@@ -24,12 +71,12 @@ const App = () => {
         {todos.map((todo, index) => {
           return <p key={index}>{todo}</p>;
         })}
-        <button onClick={addTodo}>Add Todo</button>
+        <button id="add-todo-btn" onClick={addTodo}>Add Todo</button> {/* Added id */}
       </div>
       <hr />
       <div>
         Count: {count}
-        <button onClick={increment}>+</button>
+        <button id="incr-cnt" onClick={increment}>+</button> {/* Added id */}
         <h2>Expensive Calculation</h2>
         {calculation}
       </div>
